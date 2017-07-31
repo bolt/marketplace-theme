760d680063dcad3c8bdb4de9492774c21ec252c6
Gawain Lynch <gawain.lynch@gmail.com>
2017-07-31 14:46:37 +0200

    Replace ZeroClipboard with the Flash free clipboard.js

--- js/extension-extras.js
+++ js/extension-extras.js
@@ -1,26 +1,20 @@
-jQuery(document).ready(
-    function($) {
-        $('.popup').popup({position: 'bottom left'});
-        $('.ui.toggle.button').popup();
-        $('.ui.accordion').accordion();
-        $('.scrollbar-inner').scrollbar();
-
-        var client = new ZeroClipboard(document.getElementById('copy-button'));
-        client.on(
-            'ready', function(readyEvent) {
-            }
-        );
-
-        $.ajax(
-            {
-                url: '/view/releases/' + packageID,
-                context: $('.releases-wrapper')
-            }
-        ).done(
-            function(response) {
-                $(this).html(response);
-                $('.ui.accordion').accordion();
-            }
-        );
-    }
-);
+document.addEventListener('DOMContentLoaded', function(){
+    new Clipboard('#copy-button');
+});
+jQuery(document).ready(function($) {
+    $('.popup').popup({position: 'bottom left'});
+    $('.ui.toggle.button').popup();
+    $('.ui.accordion').accordion();
+    $('.scrollbar-inner').scrollbar();
+    $.ajax(
+        {
+            url: '/view/releases/' + packageID,
+            context: $('.releases-wrapper')
+        }
+    ).done(
+        function(response) {
+            $(this).html(response);
+            $('.ui.accordion').accordion();
+        }
+    );
+});
